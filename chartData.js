3bbfee6c3a2f20f2d3793c2945c5cdf2b65e8e62
Ula <urszula.fratczakk@gmail.com>
2016-03-10 20:03:52 +0100

    First commit

---
+++ chartData.js
@@ -0,0 +1,75 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var c = document.getElementById("myChart");
+var ctx = c.getContext("2d");
+
+    var mon = document.getElementById('mon').value;
+    var tue = document.getElementById('tue').value;
+    var wed = document.getElementById('wed').value;
+    var thu = document.getElementById('thu').value;
+    var fri = document.getElementById('fri').value;
+    var sat = document.getElementById('sat').value;
+    var sun = document.getElementById('sun').value;
+
+    var data = {
+        labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+        datasets: [
+            {
+                label: "My Third dataset",
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [mon, tue, wed, thu, fri, sat, sun]
+            }
+        ]
+    };
+
+    var myLineChart = new Chart(ctx).Line(data);       
+
+function updateChart(id){
+    var number;
+    var value;
+    switch(id){
+        case "mon":
+            value = document.getElementById('mon').value;
+            number = 0;
+            break;
+        case "tue": 
+            value = document.getElementById('tue').value;
+            number = 1; 
+            break;
+        case "wed":
+            value = document.getElementById('wed').value;
+            number = 2; 
+            break;
+        case "thu": 
+            value = document.getElementById('thu').value;
+            number = 3; 
+            break;
+        case "fri":
+            value = document.getElementById('fri').value;
+            number = 4; 
+            break;
+        case "sat":
+            value = document.getElementById('sat').value;
+            number = 5; 
+            break;
+        case "sun":
+            value = document.getElementById('sun').value;
+            number = 6; 
+            break;
+    }
+    if(isNaN(value) || value=='')
+        value=0;
+    myLineChart.datasets[0].points[number].value = value;
+    myLineChart.update();
+
+}
+
+
